3aeee1f0257fe4473d908e0c8eb4854c8184416b
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-26 18:22:00 -0500

    working on Resource.js

--- src/Resource.js
+++ src/Resource.js
@@ -15,6 +15,13 @@ class Resources {
     Object.keys(this.toLoad).forEach((key) => {
       const img = new Image();
       img.src = this.toLoad[key];
+      this.images[key] = {
+        image: img,
+        isLoaded: false,
+      };
+      img.onload = () => {
+        this.images[key].isLoaded = true;
+      };
     });
   }
 }
