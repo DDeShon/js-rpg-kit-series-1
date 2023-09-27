a812eba44c1c0107d01895668d02aeadc65de768
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-27 12:28:08 -0500

    working on Sprite.js

--- Sprite.js
+++ Sprite.js
@@ -1,11 +1,19 @@
 export class Sprite {
   constructor({
-    resource,
-    frameSize,
-    hFrames,
-    vFrames,
-    frame,
-    scale,
-    position,
-  }) {}
+    resource, // image to draw
+    frameSize, // size of the image crop
+    hFrames, // horizontal frames
+    vFrames, // vertical frames
+    frame, // which frame to show
+    scale, // size of the image
+    position, // location of the image on screen
+  }) {
+    this.resource = resource;
+    this.frameSize = frameSize;
+    this.hFrames = hFrames ?? 1;
+    this.vFrames = vFrames ?? 1;
+    this.frame = frame ?? 0;
+    this.scale = scale ?? 1;
+    this.position = position;
+  }
 }
