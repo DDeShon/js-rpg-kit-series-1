db892c6303bf5ea8cfa965aec47b45618eecb6e6
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-28 14:55:21 -0500

    working on Sprite.js

--- main.js
+++ main.js
@@ -16,8 +16,17 @@ const groundSprite = new Sprite({
   frameSize: new Vector2(320, 180),
 });
 
+const hero = new Sprite({
+  resource: resources.images.hero,
+  frameSize: new Vector2(32, 32),
+  hFrames: 3,
+  vFrames: 8,
+  frame: 1,
+});
+
 const draw = () => {
   skySprite.drawImage(ctx, 0, 0);
+  groundSprite.drawImage(ctx, 0, 0);
 };
 
 const hero = new Sprite({
