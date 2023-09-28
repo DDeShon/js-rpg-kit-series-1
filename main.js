35e11fc5390cddeb4b29917c18196609599eb12c
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-28 15:22:05 -0500

    working on Sprite

--- main.js
+++ main.js
@@ -24,6 +24,11 @@ const hero = new Sprite({
   frame: 1,
 });
 
+const shadow = new Sprite({
+  resource: resources.images.shadow,
+  frameSize: new Vector2(32, 32),
+});
+
 const heroPos = new Vector2(16 * 6, 16 * 5);
 
 const draw = () => {
@@ -35,7 +40,8 @@ const draw = () => {
   const heroPosX = heroPos.x + heroOffset.x;
   const heroPosY = heroPos.y + heroOffset.y;
 
-  hero.drawImage(ctx, heroPos.x, heroPos.y);
+  shadow.drawImage(ctx, heroPosX, heroPosY);
+  hero.drawImage(ctx, heroPosX, heroPosY);
 };
 
 setInterval(() => {
