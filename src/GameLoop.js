81e924f56b4022b3f3aee2f1cafd4c383a07886a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-29 13:01:02 -0500

    working on GameLoop.js

--- src/GameLoop.js
+++ src/GameLoop.js
@@ -1,3 +1,35 @@
 export class GameLoop {
-  constructor(update, render) {}
+  constructor(update, render) {
+    this.lastFrameTime = 0;
+    this.accumulatedTime = 0;
+    this.timeStep = 1000 / 60; // 60 fps
+
+    this.update = update;
+    this.render = render;
+
+    this.rafId = null; // request animation frame ID
+    this.isRunning = false;
+  }
+
+  mainLoop = (timestamp) => {
+    if (!this.isRunning) return;
+
+    let deltaTime = timestamp - this.lastFrameTime;
+    this.lastFrameTime = timestamp;
+
+    // accumulate all the time since the last frame
+    this.accumulatedTime += deltaTime;
+
+    // fixed time step updates
+    // if there's enough accumulated time to run one or more fixed updates
+    while (this.accumulatedTime >= this.timeStep) {
+      this.update(this.timeStep); // pass the fixed time step
+      this.accumulatedTime -= this.timeStep;
+    }
+
+    // Render
+    this.render();
+
+    this.rafId = requestAnimationFrame(this.mainLoop);
+  };
 }
