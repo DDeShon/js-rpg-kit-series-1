971578dd940470f323db338129f42c938384357f
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-29 13:13:38 -0500

    working on Input.js

--- src/Input.js
+++ src/Input.js
@@ -1,5 +1,27 @@
+export const LEFT = "LEFT";
+export const RIGHT = "RIGHT";
+export const UP = "UP";
+export const DOWN = "DOWN";
+
 export class Input {
-  constructor() {}
+  constructor() {
+    this.heldDirections = [];
+    document.addEventListener("keydown", (e) => {
+      // check for dedicated direction list
+      if (e.code === "ArrowUp" || e.code === "KeyW") {
+        this.onArrowPressed(UP);
+      }
+      if (e.code === "ArrowDown" || e.code === "KeyS") {
+        this.onArrowPressed(DOWN);
+      }
+      if (e.code === "ArrowLeft" || e.code === "KeyA") {
+        this.onArrowPressed(LEFT);
+      }
+      if (e.code === "ArrowRight" || e.code === "KeyD") {
+        this.onArrowPressed(RIGHT);
+      }
+    });
+  }
 
   onArrowPressed(direction) {}
 
