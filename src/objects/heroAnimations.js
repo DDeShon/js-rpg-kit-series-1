418b0301bff8eaac24a315d16eadc57342ad3edc
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-10-02 16:35:57 -0500

    working on player animation

---
+++ src/objects/heroAnimations.js
@@ -0,0 +1,21 @@
+const walk = {
+  duration: 400,
+  frames: [
+    {
+      time: 0,
+      frame: 0,
+    },
+    {
+      time: 100,
+      frame: 1,
+    },
+    {
+      time: 200,
+      frame: 2,
+    },
+    {
+      time: 300,
+      frame: 3,
+    },
+  ],
+};
