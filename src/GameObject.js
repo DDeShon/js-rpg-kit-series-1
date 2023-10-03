1e0513406177bbc4d39cb039291b0bc86e72684d
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-10-03 16:42:23 -0500

    working on game object class

---
+++ src/GameObject.js
@@ -0,0 +1,17 @@
+import { Vector2 } from "./Vector2";
+
+export class GameObject {
+  constructor({ position }) {
+    this.position = position ?? new Vector2(0, 0);
+    this.children = [];
+  }
+
+  // first entry point of the loop
+  stepEntry(delta, root) {
+    // call updates on all children first
+    this.children.forEach((child) => child.stepEntry(delta, root));
+  }
+
+  // called once every frame
+  step(_delta) {}
+}
