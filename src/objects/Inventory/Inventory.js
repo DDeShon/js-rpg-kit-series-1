4b14140662d3d7e1bbd188f3f3f8e95b0eddbfe5
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-10-07 22:26:52 -0500

    creating Inventory.js

--- src/objects/Inventory/Inventory.js
+++ src/objects/Inventory/Inventory.js
@@ -1,13 +1,22 @@
+import { events } from "../../Events.js";
 import { GameObject } from "../../GameObject.js";
 import { resources } from "../../Resource.js";
 import { Sprite } from "../../Sprite.js";
+import { Vector2 } from "../../Vector2.js";
 
 export class Inventory extends GameObject {
   constructor() {
-    super({});
-    const sprite = new Sprite({
-      resource: resources.images.rod,
+    super({
+      position: new Vector2(0, 2),
     });
-    this.addChild(sprite);
+
+    // reacto to hero picking up an item
+    events.on("HERO_PICKS_UP_ITEM", this, (data) => {
+      // show something on the screen
+      const sprite = new Sprite({
+        resource: resources.images.rod,
+      });
+      this.addChild(sprite);
+    });
   }
 }
